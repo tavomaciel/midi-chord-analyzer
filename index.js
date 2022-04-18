63f1d9086de11881784495c988f64c1b544a20c1
Gustavo Maciel <gmaciel@atlassian.com>
2022-04-18 12:10:15 +1000

    Note names on keyboard; Code organized

--- index.js
+++ index.js
@@ -1,42 +1,75 @@
-const NATURALS_PER_OCTAVE = 8;
-const TONES_PER_OCTAVE = 12;
-const MIDI_NOTES = 128;
-// Calculated on resize
-let keys = 0;
-let firstKey = 0;
-let octaves = 0;
-let octaveWidth = 0;
-let naturalKeyWidth = 0;
-let accidentalKeyWidth = 0;
-let keysOffset = 0;
+const NATURALS_PER_OCTAVE = 8
+const TONES_PER_OCTAVE = 12
+const MIDI_NOTES = 128
+const PITCH_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+const PRESSED_KEY_COLOR = "cornflowerblue"
+const KEY_STROKE_COLOR = "#223"
+// Midi access - requested below
+let midi = null
+let currentlySelectedMidiInputId = null
 
-const keysPressedChannels = {}; // Map<NoteIndex, Set<ChannelNumber>>
-for (let i = 0; i < MIDI_NOTES; ++i) {
-    keysPressedChannels[i] = new Set();
+// Rendering state - Calculated on recalcCanvas()
+let keys = 0
+let firstKey = 0
+let octaves = 0
+let octaveWidth = 0
+let naturalKeyWidth = 0
+let accidentalKeyWidth = 0
+let keysOffset = 0
+
+// Notes state
+const keysPressedChannels = [] // List<Set<ChannelNumber>>
+const keysPressed = [] // List<NoteIndex>
+
+// Elements
+const $pianoWrapper = document.getElementById("pianoWrapper")
+const $pianoCanvas = document.getElementById("pianoCanvas")
+const $midiSelect = document.getElementById("midiSelect")
+const $pressedNotesSpan = document.getElementById("pressedNotesSpan")
+const $showOctaveNames = document.getElementById("showOctaveNames")
+const $showNotesOnKeyboard = document.getElementById("showNotesOnKeyboard")
+
+// Notes
+function clearNotePresses() {
+    for (let i = 0; i < MIDI_NOTES; ++i) keysPressedChannels[i] = new Set()
+    keysPressed.length = 0
 }
 
-function pressNote(note, channel) {
-    keysPressedChannels[note].add(channel);
-    console.log(note + " pressedChannels: " + keysPressedChannels[note].size);
-    renderKeys();
+function pressKey(key, channel) {
+    keysPressedChannels[key].add(channel)
+    updateKeysPressed()
 }
-function releaseNote(note, channel) {
-    keysPressedChannels[note].delete(channel);
-    console.log(note + " pressedChannels: " + keysPressedChannels[note].size);
-    renderKeys();
+
+function releaseKey(key, channel) {
+    keysPressedChannels[key].delete(channel)
+    updateKeysPressed()
 }
-function isNotePressed(note, channel) {
-    if (!channel) {
-        // is any channel pressed
-        return keysPressedChannels[note].size > 0;
-    } else {
-        // is this specific channel pressed?
-        return keysPressedChannels[note].has(channel);
+
+/**
+ * @param {*} channel channel to check if it is pressed. When null checks if any channel is pressing that note
+ */
+function isKeyPressed(key, channel = null) {
+    return channel ? keysPressedChannels[key].has(channel) : keysPressedChannels[key].size > 0
+}
+
+function noteFriendlyName(note) {
+    let ret = PITCH_NAMES[note % TONES_PER_OCTAVE]
+    if ($showOctaveNames.checked) ret += Math.trunc(note / TONES_PER_OCTAVE) - 1
+    return ret
+}
+
+function updateKeysPressed() {
+    keysPressed.length = 0
+    let friendlyNames = ""
+    for (let i = 0; i < MIDI_NOTES; ++i) {
+        if (isKeyPressed(i)) {
+            keysPressed.push(i)
+            if (keysPressed.length == 1) friendlyNames += noteFriendlyName(i)
+            else friendlyNames += ", " + noteFriendlyName(i)
+        }
     }
+    $pressedNotesSpan.innerText = friendlyNames
 }
-
-const canvasWrapper = document.getElementById('pianoWrapper');
-const canvas = document.getElementById('pianoCanvas');
 
 function isNaturalKey(note) {
     // TODO better way of doing this?
@@ -44,118 +77,169 @@ function isNaturalKey(note) {
     // fun transform(note: Int): Boolean = (note % 12)
     //   .let { ((6 * it) / 5 - it / 10) % 2 == 0 }
     // But there might be ways of improving it, with modulo arithmetic.
-    switch(note % 12) {
-        case 0:
-        case 2:
-        case 4:
-            return true;
-        case 1:
-        case 3:
-            return false;
-        case 5:
-        case 7:
-        case 9:
-        case 11:
-            return true;
-        case 6:
-        case 8:
-        case 10:
-            return false;
+    switch (note % 12) {
+        case 0: case 2: case 4:
+            return true
+        case 1: case 3:
+            return false
+        case 5: case 7: case 9: case 11:
+            return true
+        case 6: case 8: case 10:
+            return false
     }
 }
 
+// Rendering & Canvas
 function renderKeys() {
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const ctx = $pianoCanvas.getContext("2d")
+    ctx.clearRect(0, 0, $pianoCanvas.width, $pianoCanvas.height)
+    ctx.font = (naturalKeyWidth * 0.4) + "px Arial"
+    ctx.textAlign = "center"
+    ctx.strokeStyle = KEY_STROKE_COLOR
     // Render white keys...
-    for(let i = 0; i < keys; ++i) {
-        const key = firstKey + i;
-        const octave = Math.trunc(key / TONES_PER_OCTAVE);
-        const tone = key % TONES_PER_OCTAVE;
-        if (!isNaturalKey(tone)) continue;
+    for (let i = 0; i < keys; ++i) {
+        const key = firstKey + i
+        const tone = key % TONES_PER_OCTAVE
+        if (!isNaturalKey(tone)) continue
 
-        const keyOctaveIndex = Math.trunc((5 * tone) / 8);
-        const x = octaveWidth * octave + keyOctaveIndex * naturalKeyWidth + keysOffset;
-        const height = canvas.height;
-        
-        if (isNotePressed(key)) {
-            ctx.fillStyle = 'cornflowerblue';
-        } else {
-            ctx.fillStyle = 'white';
+        const octave = Math.trunc(key / TONES_PER_OCTAVE)
+        const keyOctaveIndex = Math.trunc((5 * tone) / 8)
+        const x = octaveWidth * octave + keyOctaveIndex * naturalKeyWidth + keysOffset
+        const height = $pianoCanvas.height
+
+        ctx.fillStyle = isKeyPressed(key) ? PRESSED_KEY_COLOR : "white"
+        ctx.fillRect(x, 0, naturalKeyWidth, height)
+        ctx.strokeRect(x, 0, naturalKeyWidth, height)
+        if ($showNotesOnKeyboard.checked) {
+            ctx.fillStyle = "black"
+            ctx.fillText(noteFriendlyName(key), x + naturalKeyWidth / 2, height * 0.9)
         }
-        ctx.strokeStyle = 'black';
-        ctx.fillRect(x, 0, naturalKeyWidth, height);
-        ctx.strokeRect(x, 0, naturalKeyWidth, height);
     }
     // Render black keys...
-    for(let i = 0; i < keys; ++i) {
-        const key = firstKey + i;
-        const octave = Math.trunc(key / TONES_PER_OCTAVE);
-        const tone = key % TONES_PER_OCTAVE;
-        if (isNaturalKey(tone)) continue;
-        const keyOctaveIndex = Math.trunc(tone / 2 + 1);
-        const x = octaveWidth * octave + keyOctaveIndex * naturalKeyWidth - accidentalKeyWidth * 0.5 + keysOffset;
-        const height = canvas.height / 2.0;
+    for (let i = 0; i < keys; ++i) {
+        const key = firstKey + i
+        const tone = key % TONES_PER_OCTAVE
+        if (isNaturalKey(tone)) continue
 
-        if (isNotePressed(key)) {
-            ctx.fillStyle = 'cornflowerblue';
-        } else {
-            ctx.fillStyle = 'black';
-        }
-        ctx.strokeStyle = 'black';
-        ctx.fillRect(x, 0, accidentalKeyWidth, height);
-        ctx.strokeRect(x, 0, accidentalKeyWidth, height);
+        const octave = Math.trunc(key / TONES_PER_OCTAVE)
+        const keyOctaveIndex = Math.trunc(tone / 2 + 1)
+        const x = octaveWidth * octave + keyOctaveIndex * naturalKeyWidth + keysOffset - accidentalKeyWidth * 0.5
+        const height = $pianoCanvas.height / 2.0
+
+        ctx.fillStyle = isKeyPressed(key) ? PRESSED_KEY_COLOR : "black"
+        ctx.fillRect(x, 0, accidentalKeyWidth, height)
+        ctx.strokeRect(x, 0, accidentalKeyWidth, height)
     }
 }
 
 function recalcCanvas() {
-    const wrapperRect = canvasWrapper.getBoundingClientRect();
-    canvas.width = wrapperRect.width;
-    canvas.height = wrapperRect.height;
-    const canvasOffset = window.devicePixelRatio * 20; // 20px
+    const wrapperRect = $pianoWrapper.getBoundingClientRect()
+    $pianoCanvas.width = wrapperRect.width
+    $pianoCanvas.height = wrapperRect.height
+    // TODO Test on mobile, offset might need to be greater there
+    const canvasOffset = window.devicePixelRatio * 20 // 20px
 
-    keys = 88;
-    firstKey = 21; // A0
-    octaves = keys / TONES_PER_OCTAVE;
-    octaveWidth = (canvas.width - canvasOffset) / octaves;
-    naturalKeyWidth = octaveWidth / 7;
-    accidentalKeyWidth = naturalKeyWidth / 2.0;
-    keysOffset = -octaveWidth * firstKey / TONES_PER_OCTAVE + canvasOffset / 2;
-    // keysOffset = -octaveWidth;
-    renderKeys();
+    keys = 88
+    firstKey = 21 // A0
+    octaves = keys / TONES_PER_OCTAVE
+    octaveWidth = ($pianoCanvas.width - canvasOffset) / octaves
+    naturalKeyWidth = octaveWidth / 7
+    accidentalKeyWidth = naturalKeyWidth / 2.0
+    keysOffset = -octaveWidth * firstKey / TONES_PER_OCTAVE + canvasOffset / 2
+    renderKeys()
 }
-new ResizeObserver(recalcCanvas).observe(canvasWrapper);
-recalcCanvas();
 
-var midi = null;
+// MIDI functions
 function onMIDIMessage(event) {
-    const messageMask = 0xF0;
-    const channelMask = 0x0F;
+    const messageMask = 0xF0
+    const channelMask = 0x0F
 
     if ((event.data[0] & messageMask) == 0x80) {
-        const channel = event.data[0] & channelMask;
-        const note = event.data[1];
-        releaseNote(note, channel);
-    }
-    if ((event.data[0] & messageMask) == 0x90) {
-        const velocity = event.data[2];
-        const channel = event.data[0] & channelMask;
-        const note = event.data[1];
-        if (velocity == 0) {
-            releaseNote(note, channel);
-        } else {
-            pressNote(note, channel);
+        // Note off event
+        const channel = event.data[0] & channelMask
+        const note = event.data[1]
+        releaseKey(note, channel)
+        renderKeys()
+    } else if ((event.data[0] & messageMask) == 0x90) {
+        // Note on event
+        const channel = event.data[0] & channelMask
+        const note = event.data[1]
+        const velocity = event.data[2]
+        if (velocity == 0) releaseKey(note, channel)
+        else pressKey(note, channel)
+        renderKeys()
+    } else {
+        // Unknown event
+        let str = "Unknown event: "
+        for (let i = 0; i < event.data.length; i++) {
+            str += "0x" + event.data[i].toString(16) + " "
         }
+        console.log(str)
     }
-  }
+}
+
+function setSelectedMidiInput() {
+    const newlySelectedInputId = $midiSelect.value
+    const midiAccess = midi
+    if (!midiAccess) return
+
+    // Start by clearing all listeners
+    midiAccess.inputs.forEach(entry => { entry.onmidimessage = undefined })
+
+    if (currentlySelectedMidiInputId != newlySelectedInputId) {
+        // It changed, so clearing all current presses is a good practice
+        clearNotePresses()
+        renderKeys()
+    }
+
+    // TODO Can I access it as a map `midiAccess.inputs[id]` ?
+    midiAccess.inputs.forEach(entry => {
+        if (entry.id == newlySelectedInputId) entry.onmidimessage = onMIDIMessage
+    })
+    currentlySelectedMidiInputId = newlySelectedInputId
+}
+
+function updateMidiDeviceSelect(event) {
+    const newlySelectedPortId = event?.port?.id
+    // Clear all entries from the select
+    while ($midiSelect.firstChild) $midiSelect.removeChild($midiSelect.lastChild)
+
+    const noneOption = document.createElement("option")
+    noneOption.text = "None"
+    $midiSelect.add(noneOption)
+
+    const midiAccess = midi
+    if (!midiAccess) return
+
+    // Add new entries with the discovered devices
+    midiAccess.inputs.forEach(entry => {
+        const inputOption = document.createElement("option")
+        inputOption.text = entry.name
+        inputOption.value = entry.id
+        $midiSelect.add(inputOption)
+        // Not very stable way of picking most recent, but does the job usually
+        $midiSelect.value = entry.id
+    })
+    setSelectedMidiInput()
+}
+
 function onMIDISuccess(midiAccess) {
-  console.log("MIDI ready!");
-  midi = midiAccess;
-  midi.inputs.forEach( function(entry) { entry.onmidimessage = onMIDIMessage; });
+    console.log("MIDI ready!")
+    midi = midiAccess
+    midiAccess.onstatechange = updateMidiDeviceSelect
+    updateMidiDeviceSelect()
 }
 
 function onMIDIFailure(msg) {
-  console.log( "Failed to get MIDI access - " + msg );
+    console.log("Failed to get MIDI access - " + msg)
 }
 
+// Initialization
+new ResizeObserver(recalcCanvas).observe($pianoWrapper)
+$showOctaveNames.onchange = () => { updateKeysPressed(); renderKeys() }
+$showNotesOnKeyboard.onchange = renderKeys
 navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure)
+$midiSelect.onchange = setSelectedMidiInput
+
+clearNotePresses()
+recalcCanvas()
